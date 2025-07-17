ec42b9eeeb4430aeb9d1483b3a308c22038b0c89
Cadillac Cylee <kylie.x.cylee.technology@gmail.com>
2025-07-17 17:56:16 +0200

    Update _Sysode.js

--- plugins/_Sysode.js
+++ plugins/_Sysode.js
@@ -17,7 +17,7 @@ smd(
    },
    async (_0x514d3c) => {
      const { exec: _0x1912df } = require("child_process");
-     _0x514d3c.reply("Shutting Down");
+     _0x514d3c.reply("Lumina is Shutting Down");
      _0x1912df("pm2 stop all");
    }
  );
@@ -31,7 +31,7 @@ smd({
   const {
     exec: _0x1912df
   } = require("child_process");
-  _0x514d3c.reply("Restarting");
+  _0x514d3c.reply("Lumina is Restarting");
   _0x1912df("pm2 restart all");
 });
 smd({
